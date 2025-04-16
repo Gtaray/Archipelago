420f00e69d2bb54bc03f3345fbb5da6a815cb6c2
George Taray <gtaray@hotmail.com>
2025-04-16 09:01:57 -0700

    Update version

--- worlds/monster_sanctuary/__init__.py
+++ worlds/monster_sanctuary/__init__.py
@@ -541,7 +541,7 @@ class MonsterSanctuaryWorld(World):
             HINTS.generate_hints(self)
 
         slot_data = {
-            "version": "1.3.3.0",
+            "version": "1.3.4.0",
             "options": {
                 "goal": self.options.goal.value,
 
